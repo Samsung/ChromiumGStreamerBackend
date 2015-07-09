5b03594a6503f251b6860170facfc5b6df3ab2cc
Julien Isorce <j.isorce@samsung.com>
2015-07-09 18:29:22 +0100

    MediaPlayerGStreamer: add support for MSE (Media Souce Extensions)
    MSE is required to play Youtube for example.
    
    WebMediaSourceGStreamer  -> blink::WebMediaSource
    WebSourceBufferGStreamer -> blink::WebSourceBuffer
    
    Almost all functions from interfaces blink::WebMediaSource
    and blink::WebSourceBuffer are translated to IPC messages.
    The Media Process acts on chromiumMediaSrc element according to
    these messages.
    
    For now encoded buffers are sent through IPC. This is also the
    case for Android but we should probably consider using shared memory.
    
    The GstBin of appsrc is inspired from WebkitMediaSource.
    But we will use multiappsrc element instead when available upstream.
    Currently seeking is not implemented but webkitgtk does not support it too.
    And it should be easier to handle it with future multiappsrc element.

---
+++ content/renderer/media/gstreamer/webmediasource_gstreamer.cc
@@ -0,0 +1,126 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/renderer/media/gstreamer/webmediasource_gstreamer.h"
+
+#include "base/guid.h"
+#include "content/renderer/media/gstreamer/webmediaplayer_gstreamer.h"
+#include "content/renderer/media/gstreamer/websourcebuffer_gstreamer.h"
+#include "media/blink/websourcebuffer_impl.h"
+#include "media/filters/chunk_demuxer.h"
+#include "third_party/WebKit/public/platform/WebCString.h"
+#include "third_party/WebKit/public/platform/WebString.h"
+
+using ::blink::WebString;
+using ::blink::WebMediaSource;
+
+namespace media {
+
+#define STATIC_ASSERT_MATCHING_STATUS_ENUM(webkit_name, chromium_name) \
+  static_assert(static_cast<int>(WebMediaSource::webkit_name) ==       \
+                    static_cast<int>(ChunkDemuxer::chromium_name),     \
+                "mismatching status enum values: " #webkit_name)
+STATIC_ASSERT_MATCHING_STATUS_ENUM(AddStatusOk, kOk);
+STATIC_ASSERT_MATCHING_STATUS_ENUM(AddStatusNotSupported, kNotSupported);
+STATIC_ASSERT_MATCHING_STATUS_ENUM(AddStatusReachedIdLimit, kReachedIdLimit);
+#undef STATIC_ASSERT_MATCHING_STATUS_ENUM
+
+WebMediaSourceGStreamer::WebMediaSourceGStreamer(
+    WebMediaPlayerGStreamer* player,
+    WebMediaPlayerMessageDispatcher* message_dispatcher,
+    const SetNetworkStateCB& set_network_state_cb)
+    : player_(player),
+      message_dispatcher_(message_dispatcher),
+      set_network_state_cb_(set_network_state_cb) {
+  DCHECK(player_);
+  DCHECK(message_dispatcher_);
+}
+
+WebMediaSourceGStreamer::~WebMediaSourceGStreamer() {}
+
+WebMediaSource::AddStatus WebMediaSourceGStreamer::addSourceBuffer(
+    const blink::WebString& type,
+    const blink::WebVector<blink::WebString>& codecs,
+    blink::WebSourceBuffer** source_buffer) {
+  std::string id = base::GenerateGUID();
+  std::vector<std::string> new_codecs(codecs.size());
+  for (size_t i = 0; i < codecs.size(); ++i)
+    new_codecs[i] = codecs[i].utf8().data();
+
+  if (!message_dispatcher_->SendAddSourceId(id, type.utf8().data(), new_codecs))
+    return WebMediaSource::AddStatusNotSupported;
+
+  WebSourceBufferGStreamer* source_buffer_gstreamer =
+      new WebSourceBufferGStreamer(id, message_dispatcher_);
+
+  *source_buffer = source_buffer_gstreamer;
+  source_buffers_[id] = source_buffer_gstreamer;
+
+  return WebMediaSource::AddStatusOk;
+}
+
+double WebMediaSourceGStreamer::duration() {
+  return player_->duration();
+}
+
+void WebMediaSourceGStreamer::setDuration(double new_duration) {
+  DCHECK_GE(new_duration, 0);
+  message_dispatcher_->SendSetDuration(
+      base::TimeDelta::FromSecondsD(new_duration));
+}
+
+void WebMediaSourceGStreamer::markEndOfStream(
+    WebMediaSource::EndOfStreamStatus) {
+  message_dispatcher_->SendMarkEndOfStream();
+}
+
+void WebMediaSourceGStreamer::unmarkEndOfStream() {
+  message_dispatcher_->SendUnmarkEndOfStream();
+}
+
+void WebMediaSourceGStreamer::OnAddSourceId(const std::string& id) {
+  if (id.empty() || source_buffers_.find(id) == source_buffers_.end())
+    set_network_state_cb_.Run(blink::WebMediaPlayer::NetworkStateFormatError);
+}
+
+void WebMediaSourceGStreamer::OnRemoveSourceId(const std::string& id) {
+  WebSourceBufferGStreamerMap::iterator iter = source_buffers_.find(id);
+  if (iter == source_buffers_.end())
+    set_network_state_cb_.Run(blink::WebMediaPlayer::NetworkStateFormatError);
+  else
+    source_buffers_.erase(iter);
+}
+
+void WebMediaSourceGStreamer::OnInitSegmentReceived(const std::string& id) {
+  WebSourceBufferGStreamerMap::iterator iter = source_buffers_.find(id);
+
+  if (iter == source_buffers_.end())
+    set_network_state_cb_.Run(blink::WebMediaPlayer::NetworkStateFormatError);
+  else
+    iter->second->OnInitSegmentReceived();
+}
+
+void WebMediaSourceGStreamer::OnBufferedRangeUpdate(
+    const std::string& id,
+    const Ranges<base::TimeDelta>& ranges) {
+  WebSourceBufferGStreamerMap::iterator iter = source_buffers_.find(id);
+
+  if (iter == source_buffers_.end())
+    set_network_state_cb_.Run(blink::WebMediaPlayer::NetworkStateFormatError);
+  else
+    iter->second->OnBufferedRangeUpdate(ranges);
+}
+
+void WebMediaSourceGStreamer::OnTimestampOffsetUpdate(
+    const std::string& id,
+    const base::TimeDelta& timestamp_offset) {
+  WebSourceBufferGStreamerMap::iterator iter = source_buffers_.find(id);
+
+  if (iter == source_buffers_.end())
+    set_network_state_cb_.Run(blink::WebMediaPlayer::NetworkStateFormatError);
+  else
+    iter->second->OnTimestampOffsetUpdate(timestamp_offset);
+}
+
+}  // namespace media
---
+++ content/renderer/media/gstreamer/webmediasource_gstreamer.h
@@ -0,0 +1,68 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_RENDERER_MEDIA_GSTREAMER_WEBMEDIASOURCE_GSTREAMER_H_
+#define CONTENT_RENDERER_MEDIA_GSTREAMER_WEBMEDIASOURCE_GSTREAMER_H_
+
+#include <string>
+#include <vector>
+
+#include "media/base/media_export.h"
+#include "media/base/media_log.h"
+#include "media/base/ranges.h"
+#include "third_party/WebKit/public/platform/WebMediaPlayer.h"
+#include "third_party/WebKit/public/platform/WebMediaSource.h"
+
+namespace media {
+class WebMediaPlayerGStreamer;
+class WebMediaPlayerMessageDispatcher;
+class WebSourceBufferGStreamer;
+
+class WebMediaSourceGStreamer : public blink::WebMediaSource {
+ public:
+  typedef base::Callback<void(blink::WebMediaPlayer::NetworkState)>
+      SetNetworkStateCB;
+
+  WebMediaSourceGStreamer(WebMediaPlayerGStreamer* player,
+                          WebMediaPlayerMessageDispatcher* message_dispatcher,
+                          const SetNetworkStateCB& set_network_state_cb);
+  virtual ~WebMediaSourceGStreamer();
+
+  // blink::WebMediaSource implementation.
+  virtual AddStatus addSourceBuffer(
+      const blink::WebString& type,
+      const blink::WebVector<blink::WebString>& codecs,
+      blink::WebSourceBuffer** source_buffer);
+  virtual double duration();
+  virtual void setDuration(double duration);
+  virtual void markEndOfStream(EndOfStreamStatus status);
+  virtual void unmarkEndOfStream();
+
+  void OnAddSourceId(const std::string& id);
+  void OnRemoveSourceId(const std::string& id);
+  void OnInitSegmentReceived(const std::string& id);
+  void OnBufferedRangeUpdate(const std::string& id,
+                             const Ranges<base::TimeDelta>& ranges);
+  void OnTimestampOffsetUpdate(const std::string& id,
+                               const base::TimeDelta& timestamp_offset);
+
+ private:
+  // WebMediaPlayerGStreamer will not be released before
+  // WebMediaSourceGStreamer.
+  WebMediaPlayerGStreamer* player_;
+  WebMediaPlayerMessageDispatcher* message_dispatcher_;
+  SetNetworkStateCB set_network_state_cb_;
+
+  typedef base::hash_map<std::string, media::WebSourceBufferGStreamer*>
+      WebSourceBufferGStreamerMap;
+  // Threading notes: |dispatchers_| is only accessed on the IO thread. Every
+  // other field is protected by |lock_|.
+  WebSourceBufferGStreamerMap source_buffers_;
+
+  DISALLOW_COPY_AND_ASSIGN(WebMediaSourceGStreamer);
+};
+
+}  // namespace media
+
+#endif  // CONTENT_RENDERER_MEDIA_GSTREAMER_WEBMEDIASOURCE_GSTREAMER_H_
