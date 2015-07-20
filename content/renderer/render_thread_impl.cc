52a02af47313e3700b04f9d606929ccab3a43da4
Anton Obzhirov <a.obzhirov@samsung.com>
2015-07-20 18:35:22 +0100

    Add DestroyChannel in RenderThreadImpl::GetMediaChannel().
    Also adds USE_GSTREAMER guards around media channel DestroyChannel() in RenderThreadImpl shutdown function.

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -948,8 +948,10 @@ void RenderThreadImpl::Shutdown() {
   if (gpu_channel_.get())
     gpu_channel_->DestroyChannel();
 
+#if defined(USE_GSTREAMER)
   if (media_channel_.get())
     media_channel_->DestroyChannel();
+#endif
 
   ChildThreadImpl::Shutdown();
 
@@ -1817,6 +1819,7 @@ MediaChannelHost* RenderThreadImpl::GetMediaChannel(
       return media_channel_.get();
 
     // Recreate the channel if it has been lost.
+    media_channel_->DestroyChannel();
     media_channel_ = NULL;
   }
 
